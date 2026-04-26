a548d895bab31fbefcf366e59250fe56957bfb01
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-04-26 00:14:15 -0600

    Added Token.Kind

--- src/token.zig
+++ src/token.zig
@@ -1,3 +1,61 @@
 const std = @import("std");
 
-pub const Token = struct {};
+pub const Token = struct {
+    kind: Kind,
+
+    const Self = @This();
+
+    pub const Kind = enum {
+        // Single-character tokens.
+        LeftParen,
+        RightParen,
+        LeftBrace,
+        RightBrace,
+        Comma,
+        Dot,
+        Minus,
+        Plus,
+        Semicolon,
+        Slash,
+        Asterisk,
+
+        // One or two character tokens.
+        Bang,
+        BangEqual,
+        Equal,
+        EqualEqual,
+        Greater,
+        GreaterEqual,
+        Less,
+        LessEqual,
+
+        // Literals.
+        Identifier,
+        String,
+        Number,
+
+        // Keywords.
+        And,
+        Class,
+        Else,
+        False,
+        Fun,
+        For,
+        If,
+        Nil,
+        Or,
+        Print,
+        Return,
+        Super,
+        This,
+        True,
+        Var,
+        While,
+
+        // Other
+        Whitespace,
+        Comment,
+        Eof,
+        Unrecognized,
+    };
+};
