996770322eb413838348703a0444837eeea4252e
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-04-26 15:46:14 -0600

    define ast

--- src/expression.zig
+++ src/expression.zig
@@ -5,3 +5,38 @@ const zlox = @import("root.zig");
 const Token = zlox.Token;
 const Lexer = zlox.Lexer;
 
+pub const Expression = union(enum) {
+    literal: Literal,
+    unary: Unary,
+    binary: Binary,
+    grouping: Grouping,
+
+    pub const Literal = Token;
+
+    pub const Unary = struct {
+        operator: Operator,
+        right_hand_side: *Expression,
+
+        pub const Operator = enum {
+            minus,
+            bang,
+        };
+    };
+
+    pub const Binary = struct {
+        operator: Operator,
+        left_hand_side: *Expression,
+        right_hand_side: *Expression,
+
+        pub const Operator = enum {
+            equal_equal,
+            bang_equal,
+            less,
+            less_equal,
+            greater,
+            greater_equal,
+        };
+    };
+
+    pub const Grouping = *Expression;
+};
