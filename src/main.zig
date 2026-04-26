5beb1c4db3ef4d35f53a62cb12f4543d3679934b
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-04-25 19:51:47 -0600

    initial commit

---
+++ src/main.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+const Io = std.Io;
+const Allocator = std.mem.Allocator;
+const log = std.log;
+
+const zlox = @import("zlox");
+
+pub fn main(init: std.process.Init) !void {
+    _=init;
+}
---
+++ src/root.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+const Io = std.Io;
+const File = std.Io.File;
+const Dir = std.Io.Dir;
+const Allocator = std.mem.Allocator;
