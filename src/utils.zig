b57503ea7ce9e2e5c9848d84e254e456d48cde23
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-04-26 00:04:17 -0600

    fixed usage of std.Io.Reader and std.Io.Writer

--- src/utils.zig
+++ src/utils.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const Io = std.Io;
 const File = std.Io.File;
 const Dir = std.Io.Dir;
+const log = std.log;
 const Allocator = std.mem.Allocator;
 const ascii = std.ascii;
 
@@ -10,31 +11,31 @@ const Lexer = zlox.Lexer;
 
 const buffer_size = 1024;
 
-fn run(io: *Io, source_code: []const u8) !void {
+fn run(io: Io, source_code: []const u8) !void {
     const lexer = Lexer.new(source_code);
     _ = lexer;
     _ = io;
     //TODO
 }
 
-pub fn runFile(io: *Io, allocator: Allocator, path: [:0]const u8) !void {
+pub fn runFile(io: Io, allocator: Allocator, path: [:0]const u8) !void {
     const file_contents = try Dir.cwd().readFileAlloc(io, path, allocator, .unlimited);
     defer allocator.free(file_contents);
     try run(io, file_contents);
 }
 
-pub fn runPrompt(io: *Io) !void {
+pub fn runPrompt(io: Io) !void {
     // initialize stdout writer
     const stdout_file = File.stdout();
-    var stdout_buffer = [_]u8{0} ** buffer_size;
-    const stdout_writer = stdout_file.writer(io, &stdout_buffer);
-    var stdout = stdout_writer.interface;
+    var stdout_buffer: [buffer_size]u8 = undefined;
+    var stdout_writer = stdout_file.writer(io, &stdout_buffer);
+    var stdout = &stdout_writer.interface;
 
-    // initialize stdout reader
+    // initialize stdin reader
     const stdin_file = File.stdin();
-    var stdin_buffer = [_]u8{0} ** buffer_size;
-    const stdin_reader = stdin_file.reader(io, &stdin_buffer);
-    var stdin = stdin_reader.interface;
+    var stdin_buffer: [buffer_size]u8 = undefined;
+    var stdin_reader = stdin_file.reader(io, &stdin_buffer);
+    var stdin = &stdin_reader.interface;
 
     while (true) {
         try stdout.print("> ", .{});
@@ -46,4 +47,11 @@ pub fn runPrompt(io: *Io) !void {
 
         try run(io, line);
     }
+}
+
+pub fn equalIgnoreAsciiCase(a: []const u8, b: []const u8) bool {
+    if (a.len != b.len) return false;
+    for (a, b) |ai, bi|
+        if (ascii.toLower(ai) != ascii.toLower(bi)) return false;
+    return true;
 }
