38fa73379a4e2cc3ede6f2ada85d7cae10d9b80c
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-04-26 17:18:23 -0600

    added toString tree walker

--- src/expression.zig
+++ src/expression.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
+const Allocator = std.mem.Allocator;
 const ascii = std.ascii;
 
 const zlox = @import("root.zig");
@@ -11,32 +13,169 @@ pub const Expression = union(enum) {
     binary: Binary,
     grouping: Grouping,
 
+    const Self = @This();
+
     pub const Literal = Token;
 
     pub const Unary = struct {
         operator: Operator,
-        right_hand_side: *Expression,
+        right_operator: *Self,
 
         pub const Operator = enum {
             minus,
             bang,
+
+            pub fn name(self: @This()) []const u8 {
+                return switch (self) {
+                    .minus => "-",
+                    .bang => "!",
+                };
+            }
         };
     };
 
     pub const Binary = struct {
         operator: Operator,
-        left_hand_side: *Expression,
-        right_hand_side: *Expression,
+        left_operand: *Self,
+        right_operator: *Self,
 
         pub const Operator = enum {
-            equal_equal,
-            bang_equal,
-            less,
-            less_equal,
-            greater,
-            greater_equal,
+            equal,
+            not_equal,
+            less_than,
+            less_than_or_equal,
+            greater_than,
+            greater_than_or_equal,
+            add,
+            subtract,
+            multiply,
+            divide,
+
+            pub fn name(self: @This()) []const u8 {
+                return switch (self) {
+                    .equal => "==",
+                    .not_equal => "!=",
+                    .less_than => "<",
+                    .less_than_or_equal => "<=",
+                    .greater_than => ">=",
+                    .greater_than_or_equal => ">",
+                    .add => "+",
+                    .subtract => "-",
+                    .multiply => "*",
+                    .divide => "/",
+                };
+            }
         };
     };
 
-    pub const Grouping = *Expression;
+    pub const Grouping = *Self;
+
+    pub fn toString(
+        self: *const Self,
+        allocator: Allocator,
+    ) Allocator.Error![]u8 {
+        var output = ArrayList(u8).empty;
+        defer output.deinit(allocator);
+
+        switch (self.*) {
+            .literal => |token| {
+                try output.appendSlice(allocator, token.lexeme);
+            },
+            .unary => |unary| {
+                const s = try parenthesize(
+                    allocator,
+                    unary.operator.name(),
+                    &.{unary.right_operator},
+                );
+                defer allocator.free(s);
+                try output.appendSlice(allocator, s);
+            },
+            .binary => |binary| {
+                const s = try parenthesize(
+                    allocator,
+                    binary.operator.name(),
+                    &.{ binary.left_operand, binary.right_operator },
+                );
+                defer allocator.free(s);
+                try output.appendSlice(allocator, s);
+            },
+            .grouping => |inner| {
+                const s = try inner.toString(allocator);
+                defer allocator.free(s);
+                try output.appendSlice(allocator, "(group ");
+                try output.appendSlice(allocator, s);
+                try output.append(allocator, ')');
+            },
+        }
+
+        return output.toOwnedSlice(allocator);
+    }
+
+    fn parenthesize(
+        allocator: Allocator,
+        name: []const u8,
+        expressions: []const *const Expression,
+    ) Allocator.Error![]u8 {
+        var output = ArrayList(u8).empty;
+        defer output.deinit(allocator);
+
+        try output.append(allocator, '(');
+        try output.appendSlice(allocator, name);
+        for (expressions) |expression| {
+            try output.append(allocator, ' ');
+            const s = try expression.toString(allocator);
+            defer allocator.free(s);
+            try output.appendSlice(allocator, s);
+        }
+        try output.append(allocator, ')');
+        return output.toOwnedSlice(allocator);
+    }
 };
+
+test "Expression.toString" {
+    _ = Expression;
+    _ = Expression.toString;
+    const e = Expression{
+        .binary = .{
+            .left_operand = a: {
+                var expr = Expression{
+                    .unary = .{
+                        .operator = .minus,
+                        .right_operator = b: {
+                            var e = Expression{
+                                .literal = .{
+                                    .kind = .Number,
+                                    .lexeme = "123",
+                                },
+                            };
+                            break :b &e;
+                        },
+                    },
+                };
+                break :a &expr;
+            },
+            .operator = .multiply,
+            .right_operator = c: {
+                var expr = Expression{
+                    .grouping = d: {
+                        var expr = Expression{
+                            .literal = .{
+                                .kind = .Number,
+                                .lexeme = "45.67",
+                            },
+                        };
+                        break :d &expr;
+                    },
+                };
+                break :c &expr;
+            },
+        },
+    };
+    const expected = "(* (- 123) (group 45.67))";
+    const actual = try e.toString(std.testing.allocator);
+    defer std.testing.allocator.free(actual);
+    std.debug.print("\n\n", .{});
+    std.debug.print("expected = {s}\n", .{expected});
+    std.debug.print("actual   = {s}\n", .{actual});
+    try std.testing.expect(std.mem.eql(u8, expected, actual));
+}
