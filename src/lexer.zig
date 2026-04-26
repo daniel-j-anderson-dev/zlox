1387e3cb7706080567bb883eb4b74d1d005784b9
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-04-26 09:24:03 -0600

    start on line number 1

--- src/lexer.zig
+++ src/lexer.zig
@@ -20,7 +20,7 @@ pub const Lexer = struct {
             .source = source,
             .lexeme_start = 0,
             .lexeme_end = 0,
-            .line_number = 0,
+            .line_number = 1,
         };
     }
 
