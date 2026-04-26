baba0466f492c777bc810c9b8d9293afc2cbe4aa
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-04-26 00:05:25 -0600

    added cli arg handling

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,41 @@ const Allocator = std.mem.Allocator;
 const log = std.log;
 
 const zlox = @import("zlox");
+const equalIgnoreAsciiCase = zlox.equalIgnoreAsciiCase;
+
+const usage_message =
+    "Usage:\n" ++
+    "Run REPL: zlox\n" ++
+    "Run a lox source file: zlox path/to/lox/source.lox";
 
 pub fn main(init: std.process.Init) !void {
-    _=init;
+    const arena = init.arena.allocator();
+    const command_line_arguments = try init.minimal.args.toSlice(arena);
+
+    const general_purpose_allocator = init.gpa;
+
+    switch (command_line_arguments.len) {
+        0, 1 => {
+            log.info("Starting lox REPL", .{});
+            try zlox.runPrompt(init.io);
+        },
+        2 => {
+            if (equalIgnoreAsciiCase(command_line_arguments[1], "usage")) {
+                log.info("{s}", .{usage_message});
+                return;
+            }
+
+            const source_file_path = command_line_arguments[1];
+            log.info("Running file: {s}", .{source_file_path});
+            try zlox.runFile(
+                init.io,
+                general_purpose_allocator,
+                command_line_arguments[1],
+            );
+        },
+        else => {
+            log.err("{s}", .{usage_message});
+            return error.TooManyArgs;
+        },
+    }
 }
