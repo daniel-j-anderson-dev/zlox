cfee50b32131955bea50e82115bf8d874ea51cb8
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-04-26 01:35:16 -0600

    handle single character tokens

--- src/lexer.zig
+++ src/lexer.zig
@@ -24,7 +24,49 @@ pub const Lexer = struct {
     }
 
     pub fn next(self: *Self) !?Token {
-        _ = self;
-        return null;
+        if (self.outOfSourceBytes()) return null;
+
+        self.startNewLexeme();
+        const current = self.currentByte();
+        self.extendLexeme();
+
+        return .{
+            .kind = switch (current) {
+                '(' => .LeftParenthesis,
+                ')' => .RightParenthesis,
+                '{' => .LeftCurlyBrace,
+                '}' => .RightCurlyBrace,
+                ',' => .Comma,
+                '.' => .Dot,
+                '-' => .Minus,
+                '+' => .Plus,
+                ';' => .Semicolon,
+                '*' => .Asterisk,
+                else => a: {
+                    break :a .Unrecognized;
+                },
+            },
+            .lexeme = self.lexeme(),
+        };
+    }
+
+    fn outOfSourceBytes(self: *const Self) bool {
+        return self.lexeme_end >= self.source.len;
+    }
+
+    fn currentByte(self: *const Self) u8 {
+        return self.source[self.lexeme_end];
+    }
+
+    fn lexeme(self: *const Self) []const u8 {
+        return self.source[self.lexeme_start..self.lexeme_end];
+    }
+
+    fn startNewLexeme(self: *Self) void {
+        self.lexeme_start = self.lexeme_end;
+    }
+
+    fn extendLexeme(self: *Self) void {
+        self.lexeme_end +|= 1;
     }
 };
